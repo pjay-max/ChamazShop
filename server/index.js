f90bab76c2d7bf20545b91f1cb8ecf794610a4b7
pjay-max <pijiwenzichamaunga@gmail.com>
2026-06-05 23:58:51 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,6 +2,27 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Connect to database
+require('./config/db');
+
+app.get('/', (req, res) => {
+  res.json({ message: 'ChamazShop API is running 🚀' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 const app = express();
 
 app.use(cors());
