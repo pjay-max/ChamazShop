44aaeba98891ad87d811db38dfa15275567a04c2
pjay-max <pijiwenzichamaunga@gmail.com>
2026-06-18 10:39:17 -0700

    Create Product.js

---
+++ server/models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  stock: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Product', productSchema);
