2ee0f5fcbbcfb87759f2de90e3e15817b722caf6
pjay-max <pijiwenzichamaunga@gmail.com>
2026-06-30 15:30:42 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home/Home'
+import Shop from './pages/Shop/Shop'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
         </Routes>
       </div>
     </Router>
